cfb8c843c3afeea9c7ffd682d480136bb2e64839
Jakcin <jakcinmanojjacobraj@devaxcel.com>
2023-03-30 11:35:00 +0530

    used event listeners a bit easy to understand

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <div>
+      <h4>Counter App</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count++">+</button>
+      <button @click="count --">-</button>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  widows: 100vw;
+}
+</style>
